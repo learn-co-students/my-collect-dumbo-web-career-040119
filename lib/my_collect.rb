97c71aac1a71cf8c18190deaada6e5454166ea44
Kate F <follett.katie@gmail.com>
2019-03-12 16:44:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
-
-
+def my_collect(collection)
+  #set up new array
+  new_array = []
+  #start i at 0
+  i = 0
+  #while loop setup
+  while i < collection.length
+    #each time it iterates we yeild orig array passed in at i ad store it
+    # in the "new array"
+    new_array << yield(collection[i])
+    #increment i
+    i += 1
+  end
+  #return the array
+  new_array
+end
