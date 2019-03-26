a351453b6a2126e5929d6a1f38c8d6054da73e1a
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-03-25 23:48:06 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect (collection)
+	i = 0
+	array = []
+	while i<collection.length
+		array << yield(collection[i])
+		i = i + 1
 
+	end
+	array
+end
 
+#how do we know if it's .upcase - that's in an external method, right?
